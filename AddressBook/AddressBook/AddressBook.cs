4b1fceb0f8a68880cb17f84ace13e4318b61574b
Shashank <langishashank@gmail.com>
2022-05-20 14:52:38 +0530

    [Shashank]:Creating the contact Details.

---
+++ AddressBook/AddressBook/AddressBook.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class AddressBook
+    {
+        public string Firstname { get; set; }
+        public string Lastname;
+        public string City;
+        public string State;
+        public string Email;
+        public string Address;
+        public int ZipCode;
+        public int PhoneNumber;
+
+    }
+}
---
+++ AddressBook/AddressBook/Program.cs
@@ -0,0 +1,60 @@
+﻿
+
+namespace AddressBook
+{
+    public class Program
+    {
+        public void program()
+        {
+
+        }
+        public static void Main(string[] args)
+        {
+            var list = new List<AddressBook>();
+            AddressBook address = new AddressBook();
+
+            Console.WriteLine(" Welcome to the Address Book ");
+
+            Console.WriteLine("Enter the First Name: ");
+            address.Firstname = Console.ReadLine();
+
+            Console.WriteLine("Enter the Last Name: ");
+            address.Lastname = Console.ReadLine();
+
+            Console.WriteLine("Enter the City Name: ");
+            address.City = Console.ReadLine();
+
+            Console.WriteLine("Enter the State Name: ");
+            address.State = Console.ReadLine();
+
+            Console.WriteLine("Enter the Email Name: ");
+            address.Email = Console.ReadLine();
+
+            Console.WriteLine("Enter the Address: ");
+            address.Address = Console.ReadLine();
+
+            Console.WriteLine("Enter the Zip Code: ");
+            address.ZipCode = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Phone Number: ");
+            address.PhoneNumber = Convert.ToInt32(Console.ReadLine());
+
+            list.Add(address);
+            Console.WriteLine("*****Details*****");
+            Console.WriteLine("FirstName: " + address.Firstname);
+            Console.WriteLine("LastName: " + address.Lastname);
+            Console.WriteLine("City: " + address.City);
+            Console.WriteLine("State: " + address.State);
+            Console.WriteLine("Email: " + address.Email);
+            Console.WriteLine("Address: " + address.Address);
+            Console.WriteLine("ZipCode: " + address.ZipCode);
+            Console.WriteLine("PhoneNumber: " + address.PhoneNumber);
+
+        }
+
+
+    }
+
+}
+
+
